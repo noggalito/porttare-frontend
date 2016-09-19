b4dcf4bad170b719e6cff6bfcb5140b9058befd7
Darwin Gualotuña <daosgc@gmail.com>
2016-09-18 20:21:39 -0500

    add slider and update template in product page

--- app/scripts/controllers/product-controller.js
+++ app/scripts/controllers/product-controller.js
@@ -8,23 +8,62 @@
   function ProductController() {
     var productVm = this;
     productVm.more = false;
+    productVm.moreLabel = 'mostrar mas';
 
     productVm.product = {
       id: 1,
       name: 'provider1',
       description: 'restaurant',
-      image: '../images/bg.png',
+      images:[ '../images/bg.png',
+        '../images/bg.png',
+        '../images/bg.png',
+        '../images/bg.png',
+        '../images/bg.png'
+      ],
       more: 'more information',
       amount: 0,
       unitPrice: 2.5,
       totalPrice: 0
     };
 
+    productVm.slickConfig = {
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
     productVm.showMore = function () {
       if(productVm.more){
         productVm.more = false;
+        productVm.moreLabel = 'mostrar mas';
       }else{
         productVm.more = true;
+        productVm.moreLabel = 'mostrar menos';
       }
     };
 
