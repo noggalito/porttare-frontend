0abd9d04e615ab5dcc9582679abae22c8db5f8f4
Mario Andrés Correa <a@macool.me>
2016-11-27 21:18:15 -0500

    setting static folder for app

--- server.js
+++ server.js
@@ -10,10 +10,10 @@
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
   }
   app.use(
-    express.static(process.env.STATIC_FOLDER || 'app')
+      express.static(process.env.STATIC_FOLDER || 'app')
   );
   app.set('port', process.env.PORT || 5001);
   app.listen(app.get('port'), function () {
-      console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
   });
 })();
