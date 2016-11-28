802ba720458056367559984345629fab41b225e0
Mario Andrés Correa <a@macool.me>
2016-11-27 21:44:12 -0500

    tweaking configs

--- config/compass-config.js
+++ config/compass-config.js
@@ -1,46 +1,46 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  var compassConfig = {
-    options: {
-      sassDir: '<%= yeoman.app %>/<%= yeoman.styles %>',
-      cssDir: '.temp/<%= yeoman.styles %>',
-      generatedImagesDir: '.temp/<%= yeoman.images %>/generated',
-      imagesDir: '<%= yeoman.app %>/<%= yeoman.images %>',
-      javascriptsDir: '<%= yeoman.app %>/<%= yeoman.scripts %>',
-      fontsDir: '<%= yeoman.app %>/<%= yeoman.styles %>/fonts',
-      importPath: '<%= yeoman.app %>/bower_components',
-      httpImagesPath: '/<%= yeoman.images %>',
-      httpGeneratedImagesPath: '/<%= yeoman.images %>/generated',
-      httpFontsPath: '/<%= yeoman.styles %>/fonts',
-      relativeAssets: false,
-      assetCacheBuster: false,
-      bundleExec: true,
-      raw: 'Sass::Script::Number.precision = 10\n',
-      specify: '<%= yeoman.app %>/<%= yeoman.styles %>/main.scss'
-    },
-    dist: {
-      options: {
-        outputStyle: 'compressed',
-        force: true,
-        cssDir: '<%= yeoman.dist %>/<%= yeoman.styles %>',
-        generatedImagesDir: '<%= yeoman.dist %>/<%= yeoman.images %>/generated'
-      }
-    },
-    staging: {
-      options: {
-        outputStyle: 'compressed',
-        force: true,
-        cssDir: '<%= yeoman.app %>/<%= yeoman.styles %>',
-        generatedImagesDir: '<%= yeoman.dist %>/<%= yeoman.images %>/generated'
-      }
-    },
-    server: {
-      options: {
-        debugInfo: true
-      }
-    }
-  };
+    var compassConfig = {
+        options: {
+            sassDir: '<%= yeoman.app %>/<%= yeoman.styles %>',
+            cssDir: '.temp/<%= yeoman.styles %>',
+            generatedImagesDir: '.temp/<%= yeoman.images %>/generated',
+            imagesDir: '<%= yeoman.app %>/<%= yeoman.images %>',
+            javascriptsDir: '<%= yeoman.app %>/<%= yeoman.scripts %>',
+            fontsDir: '<%= yeoman.app %>/<%= yeoman.styles %>/fonts',
+            importPath: '<%= yeoman.app %>/bower_components',
+            httpImagesPath: '/<%= yeoman.images %>',
+            httpGeneratedImagesPath: '/<%= yeoman.images %>/generated',
+            httpFontsPath: '/<%= yeoman.styles %>/fonts',
+            relativeAssets: false,
+            assetCacheBuster: false,
+            bundleExec: true,
+            raw: 'Sass::Script::Number.precision = 10\n',
+            specify: '<%= yeoman.app %>/<%= yeoman.styles %>/main.scss'
+        },
+        dist: {
+            options: {
+                outputStyle: 'compressed',
+                force: true,
+                cssDir: '<%= yeoman.dist %>/<%= yeoman.styles %>',
+                generatedImagesDir: '<%= yeoman.dist %>/<%= yeoman.images %>/generated'
+            }
+        },
+        staging: {
+            options: {
+                outputStyle: 'compressed',
+                force: true,
+                cssDir: '<%= yeoman.app %>/<%= yeoman.styles %>',
+                generatedImagesDir: '<%= yeoman.dist %>/<%= yeoman.images %>/generated'
+            }
+        },
+        server: {
+            options: {
+                debugInfo: true
+            }
+        }
+    };
 
-  return module.exports = compassConfig;
+    return module.exports = compassConfig;
 })();
