778186e8e24d84c5d4b06bc9d7fda428fbdc6970
Mario Andrés <a@macool.me>
2016-11-28 03:34:12 -0500

    UI enhances (#108)
    * still tweaking UI
    
    * enhance intro in landscape view in web
    
    * tweaking intro for iphone 4
    
    * tweaking UI for login
    
    * fix main content title centering
    
    * enhance styles for categories
    
    * tweaking UI
    
    * attempt to show a default username in templates

--- app/scripts/controllers/site-controller.js
+++ app/scripts/controllers/site-controller.js
@@ -1,43 +1,43 @@
 (function(){
-  'use strict';
+    'use strict';
 
-  angular
-    .module('porttare.controllers')
-    .controller('SiteController', SiteController);
+    angular
+        .module('porttare.controllers')
+        .controller('SiteController', SiteController);
 
-  function SiteController($rootScope, $ionicLoading, $auth, CartService) {
-    var siteVm = this,
-        currentUser = null;
+    function SiteController($rootScope, $ionicLoading, $auth, CartService) {
+        var siteVm = this,
+            currentUser = null;
 
-    siteVm.userName = userName;
+        siteVm.userName = userName;
 
-    $auth.validateUser()
-      .then(function userAuthorized(user) {
-        return CartService.getCart().then(function(response){
-          user.customer_order = response.customer_order; //jshint ignore:line
-          currentUser = user;
-          return currentUser;          
+        $auth.validateUser()
+            .then(function userAuthorized(user) {
+                return CartService.getCart().then(function(response){
+                    user.customer_order = response.customer_order; //jshint ignore:line
+                    currentUser = user;
+                    return currentUser;
+                });
+            });
+
+        $rootScope.$on('$stateChangeStart', function(){
+            $ionicLoading.show({
+                template: '{{::("globals.loading"|translate)}}'
+            });
         });
-      });
 
-    $rootScope.$on('$stateChangeStart', function(){
-      $ionicLoading.show({
-        template: '{{::("globals.loading"|translate)}}'
-      });
-    });
+        $rootScope.$on('$stateChangeSuccess', function () {
+            $ionicLoading.hide();
+        });
 
-    $rootScope.$on('$stateChangeSuccess', function () {
-      $ionicLoading.hide();
-    });
+        $rootScope.$on('$stateChangeError', function(){
+            $ionicLoading.hide();
+        });
 
-    $rootScope.$on('$stateChangeError', function(){
-      $ionicLoading.hide();
-    });
-
-    function userName () {
-      if (currentUser) {
-        return currentUser.name || currentUser.nickname || currentUser.email;
-      }
+        function userName () {
+            if (currentUser) {
+                return currentUser.name || currentUser.nickname || currentUser.email;
+            }
+        }
     }
-  }
 })();
