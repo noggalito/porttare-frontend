6cbb211772935ede083a513aea336543fb36df61
carito22 <caritovaldiviezo22@gmail.com>
2016-12-10 15:45:04 -0500

    remplazado por toDate()

--- app/scripts/controllers/profile/profile-info-controller.js
+++ app/scripts/controllers/profile/profile-info-controller.js
@@ -27,7 +27,7 @@
     function showNewModal() {
       piVm.userEdit = angular.copy(piVm.user);
       var fecha_nac = moment(piVm.user.fecha_nacimiento, 'YYYY/MM/DD HH:mm Z');//jshint ignore: line
-      piVm.userEdit.fecha_nacimiento = new Date(fecha_nac); //jshint ignore: line
+      piVm.userEdit.fecha_nacimiento = fecha_nac.toDate(); //jshint ignore: line
       ModalService.showModal({
         parentScope: $scope,
         fromTemplateUrl: 'templates/profile/info/edit.html'
