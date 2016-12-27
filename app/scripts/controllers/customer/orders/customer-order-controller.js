993d5a9298bf8efcb49b9d05388c38ebb9e76716
Amalia Anto <anahi2654@gmail.com>
2016-12-27 13:01:08 -0500

    billing address

--- app/scripts/controllers/customer/orders/customer-order-controller.js
+++ app/scripts/controllers/customer/orders/customer-order-controller.js
@@ -5,16 +5,23 @@
     .module('porttare.controllers')
     .controller('CustomerOrderController', CustomerOrderController);
 
-  function CustomerOrderController(customerOrder, ProfileAddressesService) {
+  function CustomerOrderController(customerOrder, ProfileAddressesService, BillingAddressesService) {
     var customerOrderVm = this;
     customerOrderVm.customerOrder = customerOrder;
     customerOrderVm.customerAddress = null;
+    customerOrderVm.customerBillingAdress = null;
 
     function init() {
+      getAddress();
+      getBillingAddress();
+    }
+
+    init();
+
+    function getAddress(){
       var customerAddressId = customerOrderVm.customerOrder.customer_address_id; // jshint ignore:line
 
       if(customerAddressId){
-
         ProfileAddressesService
           .getAddress(customerAddressId)
             .then(
@@ -25,7 +32,18 @@
       }
     }
 
-    init();
+    function getBillingAddress(){
+      var customerBillingAddressId = customerOrderVm.customerOrder.customer_billing_address_id; // jshint ignore:line
 
+      if(customerBillingAddressId){
+        BillingAddressesService
+          .getBillingAddress(customerBillingAddressId)
+            .then(
+              function(res){
+                customerOrderVm.customerBillingAddress = res;
+              }
+            );
+      }
+    }
   }
 })();
