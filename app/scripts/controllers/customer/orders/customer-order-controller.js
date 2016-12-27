7b1b67aa7ff2c4c24f835de273c2d58633350e5f
Amalia Anto <anahi2654@gmail.com>
2016-12-27 18:58:29 -0500

    ocultando nulos

--- app/scripts/controllers/customer/orders/customer-order-controller.js
+++ app/scripts/controllers/customer/orders/customer-order-controller.js
@@ -12,6 +12,8 @@
     customerOrderVm.customerOrder = customerOrder;
     customerOrderVm.customerAddress = null;
     customerOrderVm.customerBillingAdress = null;
+    customerOrderVm.getAddressLine1 = getAddressLine1;
+    customerOrderVm.getAddressLine2 = getAddressLine2;
 
     function init() {
       getAddress();
@@ -55,5 +57,32 @@
         customerOrderVm.customerOrder.totalCents = customerOrderVm.customerOrder.subtotal_items_cents + customerOrderVm.customerOrder.subtotalVATCents; // jshint ignore:line
       }
     }
+
+    function getAddressLine1(){
+      var line1 = '';
+      if(
+          customerOrderVm.customerAddress &&
+          ( customerOrderVm.customerAddress.parroquia || customerOrderVm.customerAddress.barrio )
+        ){
+        line1 += customerOrderVm.customerAddress.parroquia ? ( customerOrderVm.customerAddress.parroquia + ', ' ) :'';
+        line1 += customerOrderVm.customerAddress.barrio ? customerOrderVm.customerAddress.barrio : '';
+      }
+
+      return line1;
+    }
+
+    function getAddressLine2(){
+      var line2 = '';
+      if(
+          customerOrderVm.customerAddress &&
+          ( customerOrderVm.customerAddress.ciudad || customerOrderVm.customerAddress.codigo_postal ) // jshint ignore:line
+        ){
+        line2 += customerOrderVm.customerAddress.ciudad ? ( customerOrderVm.customerAddress.ciudad + ', ' ) :'';
+        line2 += customerOrderVm.customerAddress.codigo_postal ? customerOrderVm.customerAddress.codigo_postal : ''; // jshint ignore:line
+      }
+
+      return line2;
+    }
+
   }
 })();
